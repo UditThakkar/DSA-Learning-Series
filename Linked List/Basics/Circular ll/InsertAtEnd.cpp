97b5ccd3baf509a403d73427c9e4a9cb51c62b6d
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2021-10-10 23:22:58 +0530

    Update InsertAtEnd.cpp

--- Linked List/Basics/Circular ll/InsertAtEnd.cpp
+++ Linked List/Basics/Circular ll/InsertAtEnd.cpp
@@ -23,3 +23,19 @@ node* insertAtEnd(node* head, int x){
         return head;
     }
 }
+
+node* insertAtEndEfficient(node* head, int x){
+    node* temp = new node(x);
+    if(head==NULL){
+        temp->next = temp;
+        return temp;
+    }
+    else{
+        temp->next=head->next;
+        head->next=temp;
+        int t=head->data;
+        head->data=temp->data;
+        temp->data=t;
+        return temp;
+    }
+}
