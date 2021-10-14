a3f20970b9a8985dd31bee34329e79102fddf986
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2021-10-14 23:15:47 +0530

    Create DetectLoopinLL.cpp

---
+++ Linked List/Quetions/Easy/DetectLoopinLL.cpp
@@ -0,0 +1,20 @@
+//  que link: https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1/
+
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+        // your code here
+        Node* slow = head, *fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
