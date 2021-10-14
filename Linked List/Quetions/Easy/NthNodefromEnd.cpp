523036e92406adc760ffa5e4233bba6304fce525
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2021-10-14 20:35:05 +0530

    Update NthNodefromEnd.cpp

--- Linked List/Quetions/Easy/NthNodefromEnd.cpp
+++ Linked List/Quetions/Easy/NthNodefromEnd.cpp
@@ -16,3 +16,20 @@ int getNthFromLast(Node *head, int n)
        }
        return curr->data;
 }
+
+// EFFICIENT SOLUTION
+
+void printNthFromEnd(Node * head,int n){
+    if(head==NULL)return;
+    Node *first=head;
+    for(int i=0;i<n;i++){
+        if(first==NULL)return;
+        first=first->next;
+    }
+    Node *second=head;
+    while(first!=NULL){
+        second=second->next;
+        first=first->next;
+    }
+    cout<<(second->data);
+}
