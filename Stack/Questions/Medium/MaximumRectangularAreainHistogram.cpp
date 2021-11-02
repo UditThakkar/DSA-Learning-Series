33b5754597691261ec5c6dd6e2aaba2b7c0de2b3
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2021-11-03 01:24:44 +0530

    Update MaximumRectangularAreainHistogram.cpp

--- Stack/Questions/Medium/MaximumRectangularAreainHistogram.cpp
+++ Stack/Questions/Medium/MaximumRectangularAreainHistogram.cpp
@@ -37,3 +37,32 @@ class Solution
 # Efficient solution
 ## Time : O(n)
 
+class Solution
+{
+    public:
+    //Function to find largest rectangular area possible in a given histogram.
+    long long getMaxArea(long long arr[], int n)
+    {
+        // Your code here
+        stack <long long> s;
+        long long res=0;
+        long long tp;
+        long long curr;
+        
+        for(long long i=0;i<n;i++){
+            while(s.empty()==false && arr[s.top()]>=arr[i]){
+                tp=s.top();s.pop();
+                curr=arr[tp]* (s.empty() ? i : i - s.top() - 1);
+                res=max(res,curr);
+            }
+            s.push(i);
+        }
+        while(s.empty()==false){
+            tp=s.top();s.pop();
+            curr=arr[tp]* (s.empty() ? n : n - s.top() - 1);
+            res=max(res,curr);
+        }
+        
+        return res;
+    }
+};
