b63948930ee007da4da5133f192b431989a2927a
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2022-06-22 01:30:28 +0530

    Simple approach without reverse

--- Linked List/Quetions/Medium/AddTwoNumbersRepresentedbyLL.cpp
+++ Linked List/Quetions/Medium/AddTwoNumbersRepresentedbyLL.cpp
@@ -47,3 +47,29 @@ class Solution
         return sum;
     }
 };
+
+
+	Node *addTwoNumbers(Node *l1, Node *l2)
+{
+    // Write your code here.
+    Node* dummy = new Node(NULL);
+    Node* temp = dummy;
+    int carry = 0;
+    while(l1!=NULL or l2!=NULL or carry){
+        int sum = 0;
+        if(l1!=NULL){
+            sum+=l1->data;
+            l1 = l1->next;
+        }
+        if(l2!=NULL){
+            sum+=l2->data;
+            l2 = l2->next;
+        }
+        sum+=carry;
+        carry = sum/10;
+        Node* node = new Node(sum%10);
+        temp->next = node;
+        temp = temp->next;
+    }
+    return dummy->next;
+}
